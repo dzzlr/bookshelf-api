7a939375132023486493541c3f2a3252a55e6419
dzzlr <inigithudazzler@gmail.com>
2023-04-15 10:24:05 +0700

    init project and adding some todo list task

---
+++ src/handler.js
@@ -0,0 +1,30 @@
+const { nanoid } = require('nanoid');
+const notes = require('./notes');
+
+const addBookHandler = (request, h) => {
+  // Todo
+};
+
+const getAllBooksHandler = () => {
+  // Todo
+};
+
+const getBookByIdHandler = (request, h) => {
+  // Todo
+};
+
+const editBookByIdHandler = (request, h) => {
+  // Todo
+};
+
+const deleteBookByIdHandler = (request, h) => {
+  // Todo
+};
+
+module.exports = {
+  addBookHandler, 
+  getAllBooksHandler, 
+  getBookByIdHandler, 
+  editBookByIdHandler,
+  deleteBookByIdHandler,
+};
